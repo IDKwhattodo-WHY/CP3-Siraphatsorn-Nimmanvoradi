4799d322e5fdec92873c090c5acdb1625ee56e8c
IDKwhattodo-WHY <75434564+IDKwhattodo-WHY@users.noreply.github.com>
2021-11-06 12:12:50 +0700

    Add files via upload

---
+++ Exercise4_siraphatsorn_N.py
@@ -0,0 +1,10 @@
+FE = 83.0
+GB = 76.0
+ITCS = 72.0
+CP = 86.0
+
+print("--Your Score --")
+print("Foundation English",":",FE)
+print("General Business",":",GB)
+print("Introduction to Computer Systems",":",ITCS)
+print("Computer Programming",":",CP)
